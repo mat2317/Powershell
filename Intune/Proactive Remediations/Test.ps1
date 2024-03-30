be1a23fc81d58bfef623103f226ba34faa0d47c6
Matt Monk <mat2317@gmail.com>
2024-03-30 12:10:34 +0000

    CMTrace

--- Intune/Proactive Remediations/Test.ps1
+++ Intune/Proactive Remediations/Test.ps1
@@ -1,1 +1,4 @@
-Hello
+## Downlaod CMTracered from the following repository andd copy it in your own repo "https://github.com/andrew-s-taylor/public/raw/main/Troubleshooting/CMTrace.exe"
+$ownRepoUri = "https://github.com/andrew-s-taylor/public/raw/main/Troubleshooting/CMTrace.exe"
+
+invoke-webrequest -uri $ownRepoUri -outfile "C:\Github\CMTrace.exe"
